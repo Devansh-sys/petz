d9028e25cf3cbf9f80433926f8b208f34092b68e
vishal772004 <vishalmurthy7@gmail.com>
2026-06-05 10:28:59 +0530

    Initial commit

--- petz-backend/mvnw.cmd
+++ petz-backend/mvnw.cmd
@@ -1,6 +1,6 @@
 @echo off
 setlocal
-set MAVEN_HOME=C:\Users\2480002\AppData\Local\maven\apache-maven-3.9.6
+set MAVEN_HOME=C:\The Maven 3.9.11\apache-maven-3.9.14-bin\apache-maven-3.9.14
 set MVN_CMD=%MAVEN_HOME%\bin\mvn.cmd
 if not exist "%MVN_CMD%" (
     echo [ERROR] Maven not found at %MAVEN_HOME%
